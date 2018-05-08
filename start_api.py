f13628348a21678c53aea13e65ee1163c60acf0b
Sean Collins <secollins@linkedin.com>
2018-05-08 12:30:55 -0700

    Fix flake errors

--- start_api.py
+++ start_api.py
@@ -3,12 +3,13 @@ from flask import Flask, jsonify
 from random import randint
 app = Flask(__name__)
 
+
 @app.route('/')
 def create_kitcat_and_jsonify():
     return jsonify(
         Kitcat(age=randint(1, 13)).serialize()
     )
 
+
 if __name__ == '__main__':
     app.run()
-
