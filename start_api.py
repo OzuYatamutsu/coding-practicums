dc11d5e99efffb085af588e896f74e5eaf064daf
Sean Collins <secollins@linkedin.com>
2018-05-08 12:25:20 -0700

    Create simple API

---
+++ start_api.py
@@ -0,0 +1,14 @@
+from kitcat import Kitcat
+from flask import Flask, jsonify
+from random import randint
+app = Flask(__name__)
+
+@app.route('/')
+def create_kitcat_and_jsonify():
+    return jsonify(
+        Kitcat(age=randint(1, 13)).serialize()
+    )
+
+if __name__ == '__main__':
+    app.run()
+
