967a0f41d54ba033b5e7c464c1597f6d5e4ce611
Sean Collins <seanc@seancotech.com>
2018-06-12 20:06:46 -0700

    Checkpoint 2

--- run.py
+++ run.py
@@ -1,35 +1,53 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 from bot_logger import log
 from config import TOKEN
+from requests import get
+FOAAS_BASE_URL = 'https://www.foaas.com'
 updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
 
 
 def start_bot() -> None:
     """Connects the bot to Telegram."""
 
-    # TODO connect to Telegram here
-
     log.info("The bot have started to move!")
+    dispatcher.add_handler(CommandHandler('start', start_handler))
+    dispatcher.add_handler(CommandHandler('help', help_handler))
+    dispatcher.add_handler(CommandHandler('fuckyou', fuck_you_handler))
 
-    raise NotImplementedError
+    updater.start_polling()
 
 
 def start_handler(bot, update) -> None:
     """Handler for the /start command."""
 
+    log.info(f"User {update.message.from_user.username} ran /start.")
     bot.send_message(
         chat_id=update.message.chat_id,
-        text='INSERT_MESSAGE_HERE'
+        text='Send a friendly "Fuck You!" to your friends! Type /help for a list of fucking commands'
     )
+
+
+def fuck_you_handler(bot, update) -> None:
+    """Handler for the fuckyou command."""
+
+    log.info(f"User {update.message.from_user.username} ran /fuckyou.")
+    username: str = update.message.from_user.username
+
+    # TODO Query FOAAS API for string
+    # TODO send string back in chat message
+
 
 def help_handler(bot, update) -> None:
     """Handler for the /help command."""
 
+    log.info(f"User {update.message.from_user.username} ran /help.")
     bot.send_message(
         chat_id=update.message.chat_id,
         text=(
             "I'm an interface to the FOAAS API. Fuck you.\n"
             "Here are some commands I can do:\n\n"
+            "/fuckyou\n"
         )
     )
 
