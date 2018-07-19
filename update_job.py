205949f87946e19abbef29e64328e0a7bee0bf8b
Sean Collins <secollins@linkedin.com>
2018-07-18 21:27:42 -0700

    update_job for ftp exercise

---
+++ update_job.py
@@ -0,0 +1,19 @@
+from subprocess import check_output
+from platform import uname
+from time import ctime
+PATH_TO_UPDATE_FILE = '/home/infosec-trial-7/update_file'
+PAYLOAD = f"""SYSTEM INFORMATION FOR CATS.
+(Last updated at {ctime()}.)
+
+{sys_info.version}
+{sys_info.system}@{sys_info.release}
+
+Arch is {sys_info.processor}.
+Netstat output...
+
+{check_output('netstat', universal_newlines=True)}
+"""
+
+with open(PATH_TO_UPDATE_FILE, 'w') as f:
+    f.write(PAYLOAD)
+
