6a48c79269c43b42e0d16646e26b07c43a046d6a
Sean Collins <secollins@linkedin.com>
2018-07-18 21:28:52 -0700

    Forgot to assign var

--- update_job.py
+++ update_job.py
@@ -2,6 +2,8 @@ from subprocess import check_output
 from platform import uname
 from time import ctime
 PATH_TO_UPDATE_FILE = '/home/infosec-trial-7/update_file'
+sys_info = uname()
+
 PAYLOAD = f"""SYSTEM INFORMATION FOR CATS.
 (Last updated at {ctime()}.)
 
