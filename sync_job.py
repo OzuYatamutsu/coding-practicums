791f7057722128be30d9316f7dba1d0cca6eac16
Sean Collins <secollins@linkedin.com>
2018-07-18 21:51:56 -0700

    "Daemonize"

--- sync_job.py
+++ sync_job.py
@@ -1,8 +1,12 @@
 from ftplib import FTP
+from time import sleep
 
-with FTP('trial-ubuserv.infosec-trials.steakscorp.org') as ftp:
-    ftp.login(user='infosec-trial-7', passwd='nuAoAl]yJZa')
-    print(ftp.getwelcome())
-    print(ftp.set_pasv(True))
-    print(ftp.pwd())
-    print(ftp.sendcmd('STAT update_file'))
+while True:
+    with FTP('trial-ubuserv.infosec-trials.steakscorp.org') as ftp:
+        ftp.login(user='infosec-trial-7', passwd='nuAoAl]yJZa')
+        print(ftp.getwelcome())
+        print(ftp.set_pasv(True))
+        print(ftp.pwd())
+        print(ftp.sendcmd('STAT update_file'))
+    sleep(10)
+
