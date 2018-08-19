63e4546246cc287a0c34d5863b7c1293fc492528
Sean Collins <secollins@linkedin.com>
2018-08-18 17:20:04 -0700

    Remove FK

--- Trial2p/non_normalized_table_ddl.sql
+++ Trial2p/non_normalized_table_ddl.sql
@@ -1,43 +1,40 @@
 CREATE TABLE Current_Locations (
-    location VARCHAR2(255) NOT NULL,
-    first_name VARCHAR2(255) NOT NULL,
-    last_name VARCHAR2(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (location, first_name, last_name)
 );
 
 CREATE TABLE Operatives (
-    first_name VARCHAR2(255) NOT NULL,
-    last_name VARCHAR2(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     age_years TINYINT UNSIGNED,
     weight_kg FLOAT UNSIGNED,
     num_assignments INTEGER UNSIGNED,
-    current_location VARCHAR2(255),
+    current_location VARCHAR(255),
 
     PRIMARY KEY (first_name, last_name),
     FOREIGN KEY (current_location)
-        REFERENCES Current_Location(location)
+        REFERENCES Current_Locations(location)
         ON DELETE SET NULL
 );
 
 CREATE TABLE Assignments (
     assignment_date DATE NOT NULL,
-    location VARCHAR2(255) NOT NULL,
-    first_name VARCHAR2(255) NOT NULL,
-    last_name VARCHAR2(255) NOT NULL,
+    location VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     was_successful BOOLEAN,
     num_people_on_assignment TINYINT UNSIGNED,
 
-    PRIMARY KEY (assignment_date, location),
-    FOREIGN KEY (location)
-        REFERENCES Current_Location(location)
-        ON DELETE SET NULL
+    PRIMARY KEY (assignment_date, location)
 );
 
 CREATE TABLE Expense_Accounts (
     account_id INTEGER UNSIGNED NOT NULL,
-    first_name VARCHAR2(255) NOT NULL,
-    last_name VARCHAR2(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (account_id, first_name, last_name),
     CONSTRAINT is_valid_account_id
