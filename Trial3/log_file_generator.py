8785335a311ef328f06cfa684fa8755739a177d3
Sean Collins <seanc@seancotech.com>
2018-09-05 19:47:15 -0700

    Complete syslog simulator

--- Trial3/log_file_generator.py
+++ Trial3/log_file_generator.py
@@ -1,5 +1,8 @@
+from random import randint, choice
 from time import ctime, sleep
 from sys import argv
+LOG_SOURCE = 'sample_syslog'
+LOG_CONTENTS = []
 
 
 def main(output_file='syslog_sim.log', interval_secs=2) -> None:
@@ -7,6 +10,11 @@ def main(output_file='syslog_sim.log', interval_secs=2) -> None:
     Creates a simulated log file, appending new lines
     to it every interval_secs.
     """
+    
+    global LOG_CONTENTS
+    print(f"Loading sample log... {LOG_SOURCE}")
+    with open(LOG_SOURCE, 'r') as f:
+        LOG_CONTENTS = f.readlines() 
 
     print(f"Writing to log file: {output_file}")
 
@@ -14,21 +22,23 @@ def main(output_file='syslog_sim.log', interval_secs=2) -> None:
         with open(output_file, 'a+') as f:
             log_line = simulate_log_line()
             print(f"Writing log line: {log_line}")
-            f.write(f"{log_line}\n")
+            f.write(f"{log_line}")
 
         sleep(2)
 
 
 def simulate_log_line() -> str:
-    """Generates a log line for a random running process."""
+    """
+    Generates a log line for a random running process.
+    Sources from LOG_SOURCE.
+    """
+
+    global LOG_CONTENTS
 
     timestamp = ctime()
-    system_name = "localhost"  # TODO get from system
-    process_name = "log_file_generator.py"  # TODO get process from system
-    process_pid = 1234  # TODO get PID from system
-    message = "[ACT]:[HTTPRESPONSEPARSER]:[Error]:data=0x600007a5e8d0, code=-1009, error=(null)"  # TODO sample from event log
-
-    return f"{timestamp} {system_name} {process_name}[{process_pid}]: {message}"
+    system_name = "localhost"
+    log_line = choice(LOG_CONTENTS)
+    return f"{timestamp} {system_name} {log_line}"
 
 
 if __name__ == '__main__':
